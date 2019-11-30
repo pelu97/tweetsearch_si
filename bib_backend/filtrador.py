8782d6e0f340b5cc9e9e30e8a2eb45eac55682ef
acgabriel <gabriel_alvesozorio@hotmail.com>
2019-11-30 20:07:10 -0300

    criada primeira arquitetur do backend; Tentativas de melhora da arquitetura

---
+++ bib_backend/filtrador.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 19:53:43 2019
+
+@author: gabri
+"""
+
+
+
+import pandas as pd
+from nltk.tokenize import word_tokenize
+import json
+from textblob import TextBlob as tb
+
+#importar aqui os arquivos para leitura do banco.
+
+
+
+class filtrador():
+    
+    def filtra_por_estado(self):
+    
+    def filtra_por_municipio(self):
+        
+        
+class transformador():
+    
+    def geraDfGeolocalizado(self):
+        
---
+++ bib_backend/limpador.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 19:53:20 2019
+
+@author: gabri
+"""
+
+class limpador():
