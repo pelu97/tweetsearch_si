43f73f59db5f89de5f54798bf8d630eeb7a55945
pelu97 <kalangoandrade@yahoo.com.br>
2019-12-01 22:09:50 -0300

    tentativa de integracao do front com o backend

--- tweetsearch_site/map/load.py
+++ tweetsearch_site/map/load.py
@@ -1,13 +1,16 @@
 from .models import TweetSpot
 from django.conf import settings
 import pandas
-
+import map.buscador.buscador
+from map.bib_backend import analisadorSentimentos
+from map.bib_backend import filtrador
 
 df = 0
 
 def init():
     global df
-    df = pandas.read_json(settings.BASE_DIR + "/map/search/python.json", orient = 'records', lines = True)
+    print("reading twts.json")
+    df = pandas.read_json(settings.BASE_DIR + "/map/dados/twts.json", orient = 'records', lines = True)
 
 def delete():
     TweetSpot.objects.all().delete()
@@ -15,18 +18,12 @@ def delete():
 
 def update(new_query):
     global df
-    # new_query + ".txt"
-    # file = open(settings.BASE_DIR + "/map/search/" + new_query + ".txt", 'r')
+    df = pandas.read_json(settings.BASE_DIR + "/map/dados/twts.json", orient = 'records', lines = True)
 
+    df_proc = filtrador.transformador.geraSentimentosEstados(df)
+    #df.loc[df["created_at"].dt.date.astype(str) == new_query]
 
-    # for line in file:
-    #     splitline = line.split(';')
-    #     #substitute arguments with file data
-    #     tweet = TweetSpot(title=splitline[0], description=splitline[1], geom={ "type": "Point", "coordinates": [float(splitline[2]), float(splitline[3])]})
+    # for index, row in df_proc.iterrows():
+    #     # print(row)
+    #     tweet = TweetSpot(title = row["created_at"],  coe_sent = 0, qt_tweets = 0, geom = {"type": "Point", "coordinates": [-47.88,-15.80]})
     #     tweet.save()
-    # file.close()
-
-    for index, row in df[df["created_at"].dt.date.astype(str) == new_query].iterrows():
-        # print(row)
-        tweet = TweetSpot(title = row["created_at"],  coe_sent = 0, qt_tweets = 0, geom = {"type": "Point", "coordinates": [-47.880322,-15.8079598]})
-        tweet.save()
