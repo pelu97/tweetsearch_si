90728b30c711583a1377d21a9d12f5ac35200472
pelu97 <kalangoandrade@yahoo.com.br>
2019-12-01 22:02:22 -0300

    adicionado back end para realizar integracao

---
+++ tweetsearch_site/map/bib_backend/filtrador.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 19:53:43 2019
+
+@author: gabri
+"""
+
+
+
+import pandas as pd
+import json
+import unidecode
+from textblob import TextBlob as tb
+from django.conf import settings
+
+#importar aqui os arquivos para leitura do banco.
+
+estados = pd.read_csv(settings.BASE_DIR + '/map/dados/estados.csv')
+municipios = pd.read_csv(settings.BASE_DIR + '/map/dados/municipios.csv')
+
+estados['nome'] = estados['nome'].apply(unidecode.unidecode).str.lower()
+municipios['nome'] = municipios['nome'].apply(unidecode.unidecode).str.lower()
+
+class filtrador():
+
+    def filtra_por_estado(df, ufEstado):
+
+        localizacoes = None
+
+        for user in df.user:
+
+            if user.get('location', {}) is None:
+                aux = pd.DataFrame(data = [
+                                            ''
+                                        ],
+                                       columns = ['localizacao'])
+                localizacoes = pd.DataFrame.append(localizacoes, aux)
+                continue
+
+            aux = pd.DataFrame(data = [
+                                            user.get('location', {}).split(',')[0]
+                                        ],
+                                       columns = ['localizacao'])
+            localizacoes = pd.DataFrame.append(localizacoes, aux, ignore_index = True)
+
+        localizacoes['localizacao'] = localizacoes['localizacao'].apply(unidecode.unidecode).str.lower()
+
+        estado = estados[estados.uf == ufEstado]
+        resultado = None
+
+        for i, row in localizacoes.iterrows():
+
+            if localizacoes.at[i, 'localizacao'] == '':
+                continue
+
+            positivo = 0
+            positivo = (estado['nome'] == localizacoes.at[i, 'localizacao'])
+            positivo = positivo +  municipios[municipios.codigo_uf.values == estado.codigo_uf.values].nome.str.contains(localizacoes.at[i,'localizacao']).sum()
+
+
+            if positivo.values[0] != 0:
+                aux= pd.DataFrame(data = [
+                                            df.loc[i]
+                                        ],
+                                       columns = df.columns)
+                resultado = pd.DataFrame.append(resultado, aux, ignore_index = True)
+
+        return resultado
+
+    def filtra_por_municipio(df):
+        return
+
+class transformador():
+
+    def geraSentimentosEstados(df):
+
+            resultado = None
+
+            for i, row in estados.iterrows():
+
+                aux = filtrador.filtra_por_estado(df = df, ufEstado = estados.at[i, 'uf'])
+
+                if aux is None:
+                    continue
+
+                coef = analisadorSentimentos.analisaSentimentosTwts(df = aux)
+
+                aux2 = pd.DataFrame(data = [[
+                            estados.at[i, 'nome'],
+                            estados.at[i, 'uf'],
+                            estados.at[i, 'lon'],
+                            estados.at[i, 'lat'],
+                            len(aux.index),
+                            coef
+                            ]],
+                        columns = ['localizacao', 'uf', 'longitude', 'latitude', 'qtd_twts', 'coeficiente_de_sentimento']
+
+                        )
+
+                resultado = pd.DataFrame.append(resultado, aux2, ignore_index = True)
+
+
+            return resultado
---
+++ tweetsearch_site/map/main.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 09:27:16 2019
+
+@author: gabri
+"""
+
+from buscador import buscador
+
+import tweepy as tweepy
+from tweepy import OAuthHandler
+import pandas as pd
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+                              
+auth = tweepy.OAuthHandler(
+        
+        'hekIG6uXiTvaMQZSNTmBWigMp' , 
+        'Si2UhKyt6ZfDaaK4JZ8M1vptT409KvkgLT80RVyrez4qAdyUyc' 
+        
+        )
+
+auth.set_access_token(
+        
+        '1142224560974180353-T86fpnAPEuSsJU9jQUkmUJQkaWZZKs', 
+        'crQ8l36d7Sxpux8TM7X5ZEfF1lRnJS0mt4zkbfULdXUAt'
+        
+        )
+
+api = tweepy.API(auth, wait_on_rate_limit=True,
+                 wait_on_rate_limit_notify=True, 
+                 compression=True, 
+                 retry_count=9080, 
+                 retry_delay= 15)
+
+class MyListener(StreamListener):
+ 
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#dengue', 'dengue'], languages =['pt'])
+                             
+import pandas as pd
+from nltk.tokenize import word_tokenize
+ 
+
+df = pd.read_json("dados/twts.json", orient = 'records', lines = True) #Otima opcao de leitura
+
+word_tokenize(" ".join(df['text']), language = 'portuguese')
+
+
+
+import json
+ 
+with open('python.json', 'r') as f:
+    line = f.readline() # read only the first tweet/line
+    tweet = json.loads(line) # load it as Python dict
+    print(json.dumps(tweet, indent=4)) # pretty-print
+
+
+
+tweet['text']
+
+from textblob import TextBlob as tb
+
+count = 0
+soma = 0
+
+for teste in df.text:
+    
+    teste = tb(teste)
+    
+    teste.tags
+    
+    teste = teste.translate(to="en")
+    
+    soma = soma + teste.sentiment.polarity
+    
+    count = count + 1
+    
+print(soma/count)
+
+#pegar a localizacao
+ df.user.loc[1].get('location', {})
+
+
+
+
+
+
