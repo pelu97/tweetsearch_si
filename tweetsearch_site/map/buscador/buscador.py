500c9afeb34fe688356c7f9723db43fa04524206
pelu97 <kalangoandrade@yahoo.com.br>
2019-12-04 08:05:55 -0300

    Adicionada funcionalidade de gerar vários arquivos

--- tweetsearch_site/map/buscador/buscador.py
+++ tweetsearch_site/map/buscador/buscador.py
@@ -13,68 +13,52 @@ import sys
 
 from django.conf import settings
 
+globalkey = None
+
 auth = tweepy.OAuthHandler(
-        
-        'hekIG6uXiTvaMQZSNTmBWigMp' , 
-        'Si2UhKyt6ZfDaaK4JZ8M1vptT409KvkgLT80RVyrez4qAdyUyc' 
-        
+
+        'hekIG6uXiTvaMQZSNTmBWigMp' ,
+        'Si2UhKyt6ZfDaaK4JZ8M1vptT409KvkgLT80RVyrez4qAdyUyc'
+
         )
 
 auth.set_access_token(
-        
-        '1142224560974180353-T86fpnAPEuSsJU9jQUkmUJQkaWZZKs', 
+
+        '1142224560974180353-T86fpnAPEuSsJU9jQUkmUJQkaWZZKs',
         'crQ8l36d7Sxpux8TM7X5ZEfF1lRnJS0mt4zkbfULdXUAt'
-        
+
         )
 
 api = tweepy.API(auth, wait_on_rate_limit=True,
-                 wait_on_rate_limit_notify=True, 
-                 compression=True, 
-                 retry_count=9080, 
+                 wait_on_rate_limit_notify=True,
+                 compression=True,
+                 retry_count=9080,
                  retry_delay= 15,
                  timeout = sys.maxsize
                  )
 
 class MyListener(StreamListener):
- 
+
     def on_data(self, data):
         try:
-            with open(settings.BASE_DIR + 'dados/twts.json', 'a') as f:
+            global globalkey
+            with open(settings.BASE_DIR + 'dados/' + globalkey + '.json', 'a') as f:
                 f.write(data)
-                #O sleep eh uma solucao provisoria para a aplicacao nao cair quando houverem mtos twts 
+                #O sleep eh uma solucao provisoria para a aplicacao nao cair quando houverem mtos twts
                 sleep(0.01) #sugestao: Criar um tratamento para controlar o fluxo de dados
                 return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
         return True
- 
+
     def on_error(self, status):
         print(status)
         return True
 
 class buscaTwts():
-    
+
     def iniciaBusca(key, time):
+        global globalkey
+        globalkey = key
         twitter_stream = Stream(auth, MyListener())
         twitter_stream.filter(track=['#' + key, key], languages =['pt'])
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
-                                     
